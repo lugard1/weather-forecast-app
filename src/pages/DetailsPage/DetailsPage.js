8fd4f289e5165ce002510eda79e9aa65bfdc5e10
lugard1 <agulugard@gmail.com>
2023-03-22 20:18:23 +0100

    Add DetailsPage and HomePage

---
+++ src/pages/DetailsPage/DetailsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Details from '../../components/Details/Details';
+
+const DetailsPage = () => (
+  <section>
+    <Details />
+  </section>
+);
+
+export default DetailsPage;
